658893f2b784b7c52a4862ccbceeb0414437b4d3
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2023-11-21 21:40:06 +0100

    add faq boxes

---
+++ 12. FAQ Boxes/script.js
@@ -0,0 +1,9 @@
+let btn = document.querySelectorAll('.faq-toggle');
+
+btn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        btn.parentNode.classList.toggle('active');
+    });
+}
+);
+
